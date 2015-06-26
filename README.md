3c8276238a2b6b955da67911639cdd7409889f92
Cesar Koot <cesarkoot@Cesars-MacBook-Pro.local>
2015-06-26 22:22:17 +0200

    added code to run jetty server

---
+++ README.md
@@ -0,0 +1,1 @@
+Just run the main method and type http://localhost:8080/game in your browser.
--- src/main/java/nl/codedvalue/Main.java
+++ src/main/java/nl/codedvalue/Main.java
@@ -1,7 +1,36 @@
 package nl.codedvalue;
 
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+
 /**
  * Created by cesarkoot on 26/06/15.
  */
 public class Main {
+
+    public static void main(String[] args) throws Exception {
+        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        context.setContextPath("/");
+
+        Server jettyServer = new Server(8080);
+        jettyServer.setHandler(context);
+
+        ServletHolder jerseyServlet = context.addServlet(
+                org.glassfish.jersey.servlet.ServletContainer.class, "/*");
+        jerseyServlet.setInitOrder(0);
+
+        // Tells the Jersey Servlet which REST service/class to load.
+        jerseyServlet.setInitParameter(
+                "jersey.config.server.provider.classnames",
+                GamesResource.class.getCanonicalName());
+
+        try {
+            jettyServer.start();
+            jettyServer.join();
+        } finally {
+            jettyServer.destroy();
+        }
+    }
+
 }
